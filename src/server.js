3548090834f0f10ba9ad8cad3974eea044b2726b
Anurag Singh <94540596+CoderAnurag007@users.noreply.github.com>
2023-07-15 13:18:08 +0530

    cors issue

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://opac.netlify.app",
+  "https://opacfrontend.netlify.app/",
 ];
 
 const corsOptions = {
